a0fe8200fe1af7bc29b6d8ac4f7d10efcf7cadcb
ShadowKK69 <fabio13.sapo@sapo.pt>
2024-05-27 16:06:01 +0100

    Added extra content to Contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,18 @@ import './Contact.css';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="contact-container">
+      <h2>Let's work together!</h2>
+      <p>Contact me in case of any questions or opportunities 🚀</p>
+      <div className="contact-email">
+        <a href="mailto:fabiodaniel152015@hotmail.com">fabiodaniel152015@hotmail.com</a>
+        <i class="fa-solid fa-arrow-right"></i>
+      </div>
+      <div className="contact-socials">
+        <a href="https://linkedin.com" target="_blank" rel='noopener' ><i class="fa-brands fa-linkedin-in"></i></a>
+        <a href="https://twitter.com" target="_blank" rel='noopener' ><i class="fa-brands fa-twitter"></i></a>
+      </div>
+    </div>
   );
 }
 
